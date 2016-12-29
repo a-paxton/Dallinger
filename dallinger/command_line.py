5fdc305e1ea32eaf77f7707c567b032e1d3af042
Jesse Snyder <jsnyder@wesleyan.edu>
2016-12-29 12:56:43 -0800

    add missing config file sections while looping over sections rather than while looping over section items

--- dallinger/command_line.py
+++ dallinger/command_line.py
@@ -167,8 +167,6 @@ def setup_experiment(debug=True, verbose=False, app=None, exp_config=None):
                 # We have a dictionary key
                 section_items = exp_config.get(section_name).items()
             for key, value in section_items:
-                if getattr(local_config, local_config._to_dot_key(section_name)) is None:
-                    local_config.add_section(section_name)
                 local_config.set(section_name, key, value)
         # Re-write the experiment's local config file with the results:
         local_config.save(config_file)
